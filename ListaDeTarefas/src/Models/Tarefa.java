8da1e97f9286f8c14ece4d42d0000d0e7efef249
eduardo-dib <cardozoedudib@gmail.com>
2024-07-19 22:17:42 -0300

    Criação dos modelos de Categoria e Tarefa

---
+++ ListaDeTarefas/src/Models/Tarefa.java
@@ -0,0 +1,77 @@
+package Models;
+
+import java.time.LocalDateTime;
+
+public class Tarefa {
+
+
+    private int id;
+    private String titulo;
+    private String descricao;
+    private LocalDateTime criadoEm;
+    private Categoria categoria;
+    private String status;
+
+    public Tarefa(String titulo, String descricao, LocalDateTime criadoEm, Categoria categoria, String status) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.criadoEm = LocalDateTime.now();
+        this.categoria = categoria;
+        this.status = "Não iniciada";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDateTime getCriadoEm() {
+        return criadoEm;
+    }
+
+    public void setCriadoEm(LocalDateTime criadoEm) {
+        this.criadoEm = criadoEm;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa [id=" + id + ", titulo=" + titulo + ", descricao=" + descricao + ", criadoEm=" + criadoEm
+                + ", categoria=" + categoria + ", status=" + status + "]";
+    }
+}
